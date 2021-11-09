b605107ce6124f0d08a31a3f2ea3200978a807c9
Kingstalux <kingsleyngu25@gmail.com>
2021-11-09 08:36:05 +0100

    Add edit item

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -10,6 +10,11 @@ export default class TodoItem extends Component {
             editing: true,
           })
       }
+      handleUpdatedDone = event => {
+        if (event.key === "Enter") {
+            this.setState({ editing: false })
+          }
+      }
   render() {
     const completedStyle = {
       fontStyle: 'italic',
@@ -38,7 +43,16 @@ if (this.state.editing) {
         <button onClick={() => this.props.deleteTodoProps(id)}>Delete</button>
         <span style={completed ? completedStyle : null}>{title}</span>
           </div>
-          <input type="text" style={editMode} className={styles.textInput} />
+          <input
+  type="text"
+  style={editMode}
+  className={styles.textInput}
+  value={title}
+  onChange={e => {
+    this.props.setUpdate(e.target.value, id)
+  }}
+  onKeyDown={this.handleUpdatedDone}
+/>
       </li>
     );
   }
