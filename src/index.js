3a556227ead9445dfaf053fbdfadd9c7f2d65a6d
Kingstalux <kingsleyngu25@gmail.com>
2021-11-12 10:50:40 +0100

    Add function

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import TodoContainer from './Components/TodoContainer';
-import './App.css';
+import TodoContainer from './functionBased/Components/TodoContainer';
+import './functionBased/App.css';
 
 ReactDOM.render(
   <React.StrictMode>
